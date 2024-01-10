c12c0f487dec3b1f38131f2b93dcecf98d6e65f2
momen abdallah <momenabd392@gmail.com>
2024-01-10 19:53:45 +0300

    init

---
+++ src/main/java/com/example/harakaback/HarakaBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.harakaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HarakaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HarakaBackApplication.class, args);
+    }
+
+}
